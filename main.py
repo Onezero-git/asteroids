336bc29a2f670fef4b25e1381c19968fe9cf6d2d
Simon Martl <simonmartl45@gmail.com>
2025-07-17 06:39:02 +0200

    comment added

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def main():
         dt = clock.tick(60) / 1000
 
 
-
+# This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module
 if __name__ == "__main__":
     main()
  
